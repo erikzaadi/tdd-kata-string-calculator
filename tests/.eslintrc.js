edaef373f02a403bedb2db5d18a9cf4a41ebf066
erikzaadi <erik.zaadi@gmail.com>
2017-04-02 22:17:48 +0300

    Initial commit

---
+++ tests/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "plugins": [
+    "import"
+  ],
+  "env": {
+    "mocha": true,
+    "node": true,
+    "es6": true
+  },
+  "globals": {
+    "expect": true
+  }
+};
---
+++ tests/example.spec.js
@@ -0,0 +1,7 @@
+const sourceModule = require('../src/index.js');
+
+describe('Empty test to show folder structure', () => {
+  it('should be true', () => {
+    expect(sourceModule.TrueFunction()).to.be.true;
+  })
+});
