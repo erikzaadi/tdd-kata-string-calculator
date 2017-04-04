d9745e708d8fd3e0bc80e4eb345849bd54f5f4d4
oleg <oleg@bigpanda.io>
2017-04-04 16:15:33 +0300

    tasks 6,7

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ module.exports = {
   TrueFunction: () => true,
   Add: stringNumbers =>
     (!stringNumbers ? 0 :
-      stringNumbers.split(',')
+      stringNumbers.split('\n')
         .map(stringNum => parseInt(stringNum, 10))
         .reduce((sum, val) => val + sum, 0)),
 };
--- tests/example.spec.js
+++ tests/example.spec.js
@@ -7,23 +7,37 @@ describe('Empty test to show folder structure', () => {
 });
 
 describe('Amazong add function', () => {
-  it('should exist', () => {
-    expect(sourceModule).to.have.property('Add');
+  describe('steps 1 - 5', () => {
+    it('should exist', () => {
+      expect(sourceModule).to.have.property('Add');
+    });
+    it('should be a function', () => {
+      expect(typeof (sourceModule.Add)).to.be.eql('function');
+    });
+    it('should return a number', () => {
+      const something = sourceModule.Add();
+      expect(typeof (something)).to.be.eql('number');
+    });
+    it('should return 0 if passed an empty string', () => {
+      expect(sourceModule.Add('')).to.be.eql(0);
+    });
+    it('should return 1 of passed 1', () => {
+      expect(sourceModule.Add('1')).to.be.eql(1);
+    });
+    it('should return 3 if passed two numbers with new line between', () => {
+      expect(sourceModule.Add('1\n2')).to.be.eql(3);
+    });
   });
-  it('should be a function', () => {
-    expect(typeof (sourceModule.Add)).to.be.eql('function');
-  });
-  it('should return a number', () => {
-    const something = sourceModule.Add();
-    expect(typeof (something)).to.be.eql('number');
-  });
-  it('should return 0 if passed an empty string', () => {
-    expect(sourceModule.Add('')).to.be.eql(0);
-  });
-  it('should return 1 of passed 1', () => {
-    expect(sourceModule.Add('1')).to.be.eql(1);
-  });
-  it('should return 3 if passed 1,2', () => {
-    expect(sourceModule.Add('1,2')).to.be.eql(3);
+  describe('step 6', () => {
+    it('should handle unknown amount of numbers', () => {
+      function getRandomInt(min, max) {
+        const newMin = Math.ceil(min);
+        const newMax = Math.floor(max);
+        return Math.floor(Math.random() * (newMax - newMin)) + newMin;
+      }
+      const amount = getRandomInt(1, 10);
+      const newRandNumbers = [...Array(amount)].map(() => getRandomInt(1, 100));
+      expect(sourceModule.Add(newRandNumbers.join('\n'))).to.be.eql(newRandNumbers.reduce((sum, val) => sum + val), 0);
+    });
   });
 });
